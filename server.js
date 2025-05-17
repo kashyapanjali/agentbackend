e6ba5f0fe35fd0b6256d1e8ce0c8d66b16e74883
kashyap_anjali <133110531+kashyapanjali@users.noreply.github.com>
2025-05-17 10:31:48 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,25 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// Connect MongoDB
+mongoose
+	.connect(process.env.MONGO_URI, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	})
+	.then(() => console.log("MongoDB connected"))
+	.catch((err) => console.error(err));
+
+// Routes
+app.use("/api/auth", require("./routes/authRoutes"));
+app.use("/api/agents", require("./routes/agentRoutes"));
+app.use("/api/lists", require("./routes/listRoutes"));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on ${PORT}`));
